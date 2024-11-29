1d34e94352a49924f0c7d05353809fe97aa52fe9
Eloy Delesderrier <72226220+eloydelesderrier@users.noreply.github.com>
2024-11-29 15:25:04 -0300

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -6,11 +6,10 @@ const AboutSection=()=> {
             <div className="xl:gap-16 sm:py-16 xl:px-16" ><br/><br/><br/>
                 <h2 className="flex container lg:py-4 items-center justify-around mx-auto text-6xl font-bold text-white mb-6">Sobre mim</h2>
                 <p className=" flex container lg:py-4 items-center justify-around mx-auto text-base lg:text-lg mb-20 line-clamp-5 w-3/4">
-                 Me chamo Eloy Lisboa Delesderrier, Tenho 24 anos. Formado em Tecnologia em Análise de Desenvolvimento de
-                  Sistemas. Sempre Busco está atento as novidades do mundo Tecnologico. tenho buscado oportunidades para iniciar minha carreira
-                  na área de Tecnologia da Informação. Possuo um bom pensamento
-                  analítico, gosto de matemática e assuntos relacionados a ciênciasexatas. 
-                  Meus hobbies é jogar videogamer, amo jogar é umas das coisa que mais amo fazer. Gosto de treinar, vou quase todos dos dias para academia.
+                Me chamo Eloy Lisboa Delesderrier, tenho 24 anos. Formado em Tecnologia em Análise de Desenvolvimento de Software.
+                Sistemas. Sempre busco estar atento às novidades do mundo tecnológico. Tenho buscado oportunidades para iniciar minha carreira.
+                Na área de Tecnologia da Informação. Possuo um bom pensamento analítico, gosto de matemática e assuntos relacionados às ciências exatas.  
+                Meus hobbies são jogar videogame, amo jogar, é uma das coisas que mais gosto de fazer. Gosto de praticar esportes, correr, academia e jogar futebol.
                 </p>
             </div>
             
