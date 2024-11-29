8f806c0755ae1375ea9ff48f947c36721be549e5
Eloy Delesderrier <72226220+eloydelesderrier@users.noreply.github.com>
2024-11-29 11:25:11 -0300

    Update route.js

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -12,8 +12,8 @@ export async function POST(request, res) {
   const { email, subject, message } = await request.json();
   
   try {
-     from: `${email} <${fromEmail}>`,
-      to: [emailFrom],
+     from: fromEmail,
+      to: [fromEmail],
       subject: subject,
       react: (
         <>
@@ -21,7 +21,7 @@ export async function POST(request, res) {
           
           <h2>mensagem:<p>{message}</p></h2>
 
-          
+          <p>{email}</p>
          
         </>
       ),
